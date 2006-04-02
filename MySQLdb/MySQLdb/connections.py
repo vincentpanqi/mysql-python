22a2599f7eb726bd9eee8da84bc1824f85b7d7ae
adustman <adustman@d6de93c4-43fb-41d0-b770-b43e4475168d>
2006-04-02 17:03:52 +0000

    Fix MySQLdb release tags

--- MySQLdb/MySQLdb/connections.py
+++ MySQLdb/MySQLdb/connections.py
@@ -1,4 +1,4 @@
-"""
+"""MySQLdb Connections Module
 
 This module implements connections for MySQLdb. Presently there is
 only one class: Connection. Others are unlikely. However, you might
@@ -10,188 +10,147 @@ import cursors
 from _mysql_exceptions import Warning, Error, InterfaceError, DataError, \
      DatabaseError, OperationalError, IntegrityError, InternalError, \
      NotSupportedError, ProgrammingError
-import types, _mysql
-
 
 def defaulterrorhandler(connection, cursor, errorclass, errorvalue):
-    """
-
-    If cursor is not None, (errorclass, errorvalue) is appended to
-    cursor.messages; otherwise it is appended to
-    connection.messages. Then errorclass is raised with errorvalue as
-    the value.
-
-    You can override this with your own error handler by assigning it
-    to the instance.
-
-    """
-    error = errorclass, errorvalue
     if cursor:
-        cursor.messages.append(error)
+        cursor.messages.append(errorvalue)
     else:
-        connection.messages.append(error)
+        connection.messages.append(errorvalue)
     raise errorclass, errorvalue
 
+class Connection:
 
-class Connection(_mysql.connection):
+    """Create a connection to the database. It is strongly recommended
+    that you only use keyword parameters. "NULL pointer" indicates that
+    NULL will be passed to mysql_real_connect(); the value in parenthesis
+    indicates how MySQL interprets the NULL. Consult the MySQL C API
+    documentation for more information.
 
-    """MySQL Database Connection Object"""
+    host -- string, host to connect to or NULL pointer (localhost)
+    user -- string, user to connect as or NULL pointer (your username)
+    passwd -- string, password to use or NULL pointer (no password)
+    db -- string, database to use or NULL (no DB selected)
+    port -- integer, TCP/IP port to connect to or default MySQL port
+    unix_socket -- string, location of unix_socket to use or use TCP
+    client_flags -- integer, flags to use or 0 (see MySQL docs)
+    conv -- conversion dictionary, see MySQLdb.converters
+    connect_time -- number of seconds to wait before the connection
+            attempt fails.
+    compress -- if set, compression is enabled
+    init_command -- command which is run once the connection is created
+    read_default_file -- see the MySQL documentation for mysql_options()
+    read_default_group -- see the MySQL documentation for mysql_options()
+    cursorclass -- class object, used to create cursors or cursors.Cursor.
+            This parameter MUST be specified as a keyword parameter.
+
+    Returns a Connection object.
+
+    There are a number of undocumented, non-standard methods. See the
+    documentation for the MySQL C API for some hints on what they do.
+    """
 
     default_cursor = cursors.Cursor
     
     def __init__(self, *args, **kwargs):
-        """
-
-        Create a connection to the database. It is strongly recommended
-        that you only use keyword parameters. Consult the MySQL C API
-        documentation for more information.
-
-        host
-          string, host to connect
-          
-        user
-          string, user to connect as
-
-        passwd
-          string, password to use
-
-        db
-          string, database to use
-
-        port
-          integer, TCP/IP port to connect to
-
-        unix_socket
-          string, location of unix_socket to use
-
-        conv
-          conversion dictionary, see MySQLdb.converters
-
-        connect_time
-          number of seconds to wait before the connection attempt
-          fails.
-
-        compress
-          if set, compression is enabled
-
-        named_pipe
-          if set, a named pipe is used to connect (Windows only)
-
-        init_command
-          command which is run once the connection is created
-
-        read_default_file
-          file from which default client values are read
-
-        read_default_group
-          configuration group to use from the default file
-
-        cursorclass
-          class object, used to create cursors (keyword only)
-
-        use_unicode
-          If True, text-like columns are returned as unicode objects
-          using the connection's character set.  Otherwise, text-like
-          columns are returned as strings.  columns are returned as
-          normal strings. Unicode objects will always be encoded to
-          the connection's character set regardless of this setting.
-
-        client_flag
-          integer, flags to use or 0
-          (see MySQL docs or constants/CLIENTS.py)
-
-        ssl
-          dictionary or mapping, contains SSL connection parameters;
-          see the MySQL documentation for more details
-          (mysql_ssl_set()).  If this is set, and the client does not
-          support SSL, UnsupportedError will be raised.
-
-        local_infile
-          integer, non-zero enables LOAD LOCAL INFILE; zero disables
-    
-        There are a number of undocumented, non-standard methods. See the
-        documentation for the MySQL C API for some hints on what they do.
-
-        """
-        from constants import CLIENT, FIELD_TYPE
+        from _mysql import connect
+        from constants import CLIENT
         from converters import conversions
         import types
-        from weakref import proxy
         kwargs2 = kwargs.copy()
-        if kwargs.has_key('conv'):
-            kwargs2['conv'] = conv = kwargs['conv']
-        else:
-            kwargs2['conv'] = conv = conversions.copy()
+        if not kwargs.has_key('conv'):
+            kwargs2['conv'] = conversions.copy()
         if kwargs.has_key('cursorclass'):
             self.cursorclass = kwargs['cursorclass']
             del kwargs2['cursorclass']
         else:
             self.cursorclass = self.default_cursor
-        use_unicode = kwargs.get('use_unicode', 0)
-        if kwargs.has_key('use_unicode'):
-            del kwargs2['use_unicode']
-                
-        super(Connection, self).__init__(*args, **kwargs2)
-
-        self.charset = self.character_set_name().split('_')[0]
-
-        if use_unicode:
-            def u(s, self=proxy(self)):
-                return s.decode(self.charset)
-            conv[FIELD_TYPE.STRING] = u
-            conv[FIELD_TYPE.VAR_STRING] = u
-            conv[FIELD_TYPE.BLOB].insert(-1, (None, u))
-
-        def string_literal(obj, dummy=None, self=proxy(self)):
-            return self.string_literal(obj)
-        
-        def unicode_literal(u, dummy=None, self=proxy(self)):
-            return self.literal(u.encode(self.charset))
-         
-        self.converter[types.StringType] = string_literal
-        self.converter[types.UnicodeType] = unicode_literal
-        self._transactional = self.server_capabilities & CLIENT.TRANSACTIONS
-        if self._transactional:
-            # PEP-249 requires autocommit to be initially off
-            self.autocommit(0)
+        self._db = apply(connect, args, kwargs2)
+        self._db.converter[types.StringType] = self._db.string_literal
+        if hasattr(types, 'UnicodeType'):
+            self._db.converter[types.UnicodeType] = self.unicode_literal
+        self._transactional = self._db.server_capabilities & CLIENT.TRANSACTIONS
         self.messages = []
         
-    def cursor(self, cursorclass=None):
-        """
+    def __del__(self):
+        if hasattr(self, '_db'): self.close()
+        
+    def close(self):
+        """Close the connection. No further activity possible."""
+        self._db.close()
 
-        Create a cursor on which queries may be performed. The
+    def begin(self):
+        """Explicitly begin a transaction. Non-standard."""
+        self._db.query("BEGIN")
+        
+    def commit(self):
+        """Commit the current transaction."""
+        if self._transactional:
+            self._db.query("COMMIT")
+            
+    def rollback(self):
+        """Rollback the current transaction."""
+        if self._transactional:
+            self._db.query("ROLLBACK")
+        else:
+            raise NotSupportedError, "Not supported by server"
+            
+    def cursor(self, cursorclass=None):
+        
+        """Create a cursor on which queries may be performed. The
         optional cursorclass parameter is used to create the
         Cursor. By default, self.cursorclass=cursors.Cursor is
-        used.
+        used."""
 
-        """
         return (cursorclass or self.cursorclass)(self)
 
+    # Non-portable MySQL-specific stuff
+    # Methods not included on purpose (use Cursors instead):
+    #     query, store_result, use_result
+
     def literal(self, o):
-        """
+        """If o is a single object, returns an SQL literal as a string.
+        If o is a non-string sequences, the items of the sequence are
+        converted and returned as a sequence."""
+        import _mysql
+        return _mysql.escape(o, self._db.converter)
 
-        If o is a single object, returns an SQL literal as a string.
-        If o is a non-string sequence, the items of the sequence are
-        converted and returned as a sequence.
+    def unicode_literal(self, u, dummy=None):
+        """Convert a unicode object u to a string using the current
+        character set as the encoding. If that's not available,
+        use latin1."""
+        try: charset = self.character_set_name()
+        except: charset = 'latin1'
+        return self.literal(u.encode(charset))
+    
+    def affected_rows(self): return self._db.affected_rows()
+    def dump_debug_info(self): return self._db.dump_debug_info()
+    def escape_string(self, s): return self._db.escape_string(s)
+    def get_host_info(self): return self._db.get_host_info()
+    def get_proto_info(self): return self._db.get_proto_info()
+    def get_server_info(self): return self._db.get_server_info()
+    def info(self): return self._db.info()
+    def kill(self, p): return self._db.kill(p)
+    def field_count(self): return self._db.field_count()
+    num_fields = field_count # used prior to MySQL-3.22.24
+    def ping(self): return self._db.ping()
+    def row_tell(self): return self._db.row_tell()
+    def select_db(self, db): return self._db.select_db(db)
+    def shutdown(self): return self._db.shutdown()
+    def stat(self): return self._db.stat()
+    def string_literal(self, s): return self._db.string_literal(s)
+    def thread_id(self): return self._db.thread_id()
 
-        Non-standard. For internal use; do not use this in your
-        applications.
+    def _try_feature(self, feature, *args, **kwargs):
+        try:
+            return apply(getattr(self._db, feature), args, kwargs)
+        except AttributeError:
+            self.errorhandler(self, None, NotSupportedError,
+                              "not supported by MySQL library")
+    def character_set_name(self):
+        return self._try_feature('character_set_name')
+    def change_user(self, *args, **kwargs):
+        return apply(self._try_feature, ('change_user',)+args, kwargs)
 
-        """
-        return self.escape(o, self.converter)
-
-    if not hasattr(_mysql.connection, 'warning_count'):
-
-        def warning_count(self):
-            """Return the number of warnings generated from the
-            last query. This is derived from the info() method."""
-            from string import atoi
-            info = self.info()
-            if info:
-                return atoi(info.split()[-1])
-            else:
-                return 0
-            
     Warning = Warning
     Error = Error
     InterfaceError = InterfaceError
@@ -204,3 +163,5 @@ class Connection(_mysql.connection):
     NotSupportedError = NotSupportedError
 
     errorhandler = defaulterrorhandler
+
+    
