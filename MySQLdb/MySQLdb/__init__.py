1f4f420406f2f42edcd59132baf15ca22837069d
adustman <adustman@d6de93c4-43fb-41d0-b770-b43e4475168d>
2006-04-02 17:03:52 +0000

    Fix MySQLdb release tags

--- MySQLdb/MySQLdb/__init__.py
+++ MySQLdb/MySQLdb/__init__.py
@@ -17,10 +17,10 @@ __author__ = "Andy Dustman <andy@dustman.net>"
 __revision__ = """$Revision$"""[11:-2]
 version_info = (
     1,
-    2,
     0,
-    "final",
-    1)
+    0,
+    "gamma",
+    2)
 if version_info[3] == "final": __version__ = "%d.%d.%d" % version_info[:3]
 else: __version__ = "%d.%d.%d%1.1s%d" % version_info[:5]
 
@@ -56,14 +56,12 @@ TIMESTAMP = DBAPISet(FIELD_TYPE.TIMESTAMP, FIELD_TYPE.DATETIME)
 DATETIME  = TIMESTAMP
 ROWID     = DBAPISet()
 
-def Binary(x):
-    from array import array
-    return array('c', x)
+def Binary(x): return str(x)
 
 def Connect(*args, **kwargs):
     """Factory function for connections.Connection."""
     from connections import Connection
-    return Connection(*args, **kwargs)
+    return apply(Connection, args, kwargs)
 
 connect = Connection = Connect
 
