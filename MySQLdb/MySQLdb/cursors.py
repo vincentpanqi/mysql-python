f8ce939e784c1deaf0b5d1599a45e002b7f8ffbd
adustman <adustman@d6de93c4-43fb-41d0-b770-b43e4475168d>
2009-03-30 23:28:25 +0000

    MySQLdb-1.2.3c1

--- MySQLdb/MySQLdb/cursors.py
+++ MySQLdb/MySQLdb/cursors.py
@@ -1,25 +1,25 @@
-"""
-MySQLdb Cursors
----------------
+"""MySQLdb Cursors
 
 This module implements Cursors of various types for MySQLdb. By
 default, MySQLdb uses the Cursor class.
 
 """
 
-__revision__ = "$Revision$"[11:-2]
-__author__ = "$Author$"[9:-2]
-
 import re
-import sys
-import weakref
 
-INSERT_VALUES = re.compile(r"(?P<start>.+values\s*)"
-                           r"(?P<values>\(((?<!\\)'[^\)]*?\)[^\)]*(?<!\\)?'|[^\(\)]|(?:\([^\)]*\)))+\))"
-                           r"(?P<end>.*)", re.I)
+restr = (r"\svalues\s*"
+        r"(\(((?<!\\)'[^\)]*?\)[^\)]*(?<!\\)?'"
+        r"|[^\(\)]|"
+        r"(?:\([^\)]*\))"
+        r")+\))")
+
+insert_values= re.compile(restr)
+from _mysql_exceptions import Warning, Error, InterfaceError, DataError, \
+     DatabaseError, OperationalError, IntegrityError, InternalError, \
+     NotSupportedError, ProgrammingError
 
 
-class Cursor(object):
+class BaseCursor(object):
     
     """A base for Cursor classes. Useful attributes:
     
@@ -38,16 +38,16 @@ class Cursor(object):
 
     """
 
-    from MySQLdb.exceptions import MySQLError, Warning, Error, InterfaceError, \
+    from _mysql_exceptions import MySQLError, Warning, Error, InterfaceError, \
          DatabaseError, DataError, OperationalError, IntegrityError, \
          InternalError, ProgrammingError, NotSupportedError
-
+    
     _defer_warnings = False
-    _fetch_type = None
-
-    def __init__(self, connection, encoders):
-        from MySQLdb.converters import default_decoders
-        self.connection = weakref.proxy(connection)
+    
+    def __init__(self, connection):
+        from weakref import proxy
+    
+        self.connection = proxy(connection)
         self.description = None
         self.description_flags = None
         self.rowcount = -1
@@ -60,41 +60,33 @@ class Cursor(object):
         self._warnings = 0
         self._info = None
         self.rownumber = None
-        self._encoders = encoders
-
+        
     def __del__(self):
         self.close()
         self.errorhandler = None
         self._result = None
-
+        
     def close(self):
         """Close the cursor. No further queries will be possible."""
-        if not self.connection:
-            return
-        try:
-            while self.nextset():
-                pass
-        except:
-            pass
+        if not self.connection: return
+        while self.nextset(): pass
         self.connection = None
 
     def _check_executed(self):
-        """Ensure that .execute() has been called."""
         if not self._executed:
-            self.errorhandler(self, self.ProgrammingError, "execute() first")
+            self.errorhandler(self, ProgrammingError, "execute() first")
 
     def _warning_check(self):
-        """Check for warnings, and report via the warnings module."""
         from warnings import warn
         if self._warnings:
             warnings = self._get_db().show_warnings()
             if warnings:
                 # This is done in two loops in case
                 # Warnings are set to raise exceptions.
-                for warning in warnings:
-                    self.messages.append((self.Warning, warning))
-                for warning in warnings:
-                    warn(warning[-1], self.Warning, 3)
+                for w in warnings:
+                    self.messages.append((self.Warning, w))
+                for w in warnings:
+                    warn(w[-1], self.Warning, 3)
             elif self._info:
                 self.messages.append((self.Warning, self._info))
                 warn(self._info, self.Warning, 3)
@@ -108,26 +100,27 @@ class Cursor(object):
             self.fetchall()
         del self.messages[:]
         
-        connection = self._get_db()
-        num_rows = connection.next_result()
-        if num_rows == -1:
+        db = self._get_db()
+        nr = db.next_result()
+        if nr == -1:
             return None
         self._do_get_result()
         self._post_get_result()
         self._warning_check()
-        return True
+        return 1
 
+    def _post_get_result(self): pass
+    
     def _do_get_result(self):
-        """Get the result from the last query."""
-        connection = self._get_db()
+        db = self._get_db()
         self._result = self._get_result()
-        self.rowcount = connection.affected_rows()
+        self.rowcount = db.affected_rows()
         self.rownumber = 0
         self.description = self._result and self._result.describe() or None
         self.description_flags = self._result and self._result.field_flags() or None
-        self.lastrowid = connection.insert_id()
-        self._warnings = connection.warning_count()
-        self._info = connection.info()
+        self.lastrowid = db.insert_id()
+        self._warnings = db.warning_count()
+        self._info = db.info()
     
     def setinputsizes(self, *args):
         """Does nothing, required by DB API."""
@@ -136,14 +129,12 @@ class Cursor(object):
         """Does nothing, required by DB API."""
 
     def _get_db(self):
-        """Get the database connection.
-        
-        Raises ProgrammingError if the connection has been closed."""
         if not self.connection:
-            self.errorhandler(self, self.ProgrammingError, "cursor closed")
-        return self.connection._db
+            self.errorhandler(self, ProgrammingError, "cursor closed")
+        return self.connection
     
     def execute(self, query, args=None):
+
         """Execute a query.
         
         query -- string, query to execute on server
@@ -156,40 +147,39 @@ class Cursor(object):
         Returns long integer rows affected, if any
 
         """
+        from types import ListType, TupleType
+        from sys import exc_info
         del self.messages[:]
         db = self._get_db()
         charset = db.character_set_name()
         if isinstance(query, unicode):
             query = query.encode(charset)
+        if args is not None:
+            query = query % db.literal(args)
         try:
-            if args is not None:
-                query = query % tuple(map(self.connection.literal, args))
-            result = self._query(query)
-        except TypeError, msg:
-            if msg.args[0] in ("not enough arguments for format string",
-                               "not all arguments converted"):
-                self.messages.append((self.ProgrammingError, msg.args[0]))
-                self.errorhandler(self, self.ProgrammingError, msg.args[0])
+            r = self._query(query)
+        except TypeError, m:
+            if m.args[0] in ("not enough arguments for format string",
+                             "not all arguments converted"):
+                self.messages.append((ProgrammingError, m.args[0]))
+                self.errorhandler(self, ProgrammingError, m.args[0])
             else:
-                self.messages.append((TypeError, msg))
-                self.errorhandler(self, TypeError, msg)
+                self.messages.append((TypeError, m))
+                self.errorhandler(self, TypeError, m)
         except:
-            exc, value, traceback = sys.exc_info()
-            del traceback
+            exc, value, tb = exc_info()
+            del tb
             self.messages.append((exc, value))
             self.errorhandler(self, exc, value)
-            
         self._executed = query
-        if not self._defer_warnings:
-            self._warning_check()
-        return result
+        if not self._defer_warnings: self._warning_check()
+        return r
 
     def executemany(self, query, args):
+
         """Execute a multi-row query.
         
-        query
-        
-            string, query to execute on server
+        query -- string, query to execute on server
 
         args
 
@@ -205,51 +195,42 @@ class Cursor(object):
         """
         del self.messages[:]
         db = self._get_db()
-        if not args:
-            return
-        charset = self.connection.character_set_name()
-        if isinstance(query, unicode):
-            query = query.encode(charset)
-        matched = INSERT_VALUES.match(query)
-        if not matched:
-            self.rowcount = sum([ self.execute(query, arg) for arg in args ])
-            return self.rowcount
-        
-        start = matched.group('start')
-        end = matched.group('end')
-        values = matched.group('values')
- 
+        if not args: return
+        charset = db.character_set_name()
+        if isinstance(query, unicode): query = query.encode(charset)
+        m = insert_values.search(query)
+        if not m:
+            r = 0
+            for a in args:
+                r = r + self.execute(query, a)
+            return r
+        p = m.start(1)
+        e = m.end(1)
+        qv = m.group(1)
         try:
-            sql_params = ( values % tuple(map(self.connection.literal, row)) for row in args )
-            multirow_query = '\n'.join([start, ',\n'.join(sql_params), end])
-            self._executed = multirow_query
-            self.rowcount = int(self._query(multirow_query))
-
+            q = [ qv % db.literal(a) for a in args ]
         except TypeError, msg:
             if msg.args[0] in ("not enough arguments for format string",
                                "not all arguments converted"):
-                self.messages.append((self.ProgrammingError, msg.args[0]))
-                self.errorhandler(self, self.ProgrammingError, msg.args[0])
+                self.errorhandler(self, ProgrammingError, msg.args[0])
             else:
-                self.messages.append((TypeError, msg))
                 self.errorhandler(self, TypeError, msg)
         except:
-            exc, value, traceback = sys.exc_info()
-            del traceback
+            from sys import exc_info
+            exc, value, tb = exc_info()
+            del tb
             self.errorhandler(self, exc, value)
-        
-        if not self._defer_warnings:
-            self._warning_check()
-        return self.rowcount
+        r = self._query('\n'.join([query[:p], ',\n'.join(q), query[e:]]))
+        if not self._defer_warnings: self._warning_check()
+        return r
     
     def callproc(self, procname, args=()):
+
         """Execute stored procedure procname with args
         
-        procname
-            string, name of procedure to execute on server
+        procname -- string, name of procedure to execute on server
 
-        args
-            Sequence of parameters to use with procedure
+        args -- Sequence of parameters to use with procedure
 
         Returns the original args.
 
@@ -273,37 +254,37 @@ class Cursor(object):
         disconnected.
         """
 
+        from types import UnicodeType
         db = self._get_db()
-        charset = self.connection.character_set_name()
+        charset = db.character_set_name()
         for index, arg in enumerate(args):
-            query = "SET @_%s_%d=%s" % (procname, index,
-                                        self.connection.literal(arg))
-            if isinstance(query, unicode):
-                query = query.encode(charset)
-            self._query(query)
+            q = "SET @_%s_%d=%s" % (procname, index,
+                                         db.literal(arg))
+            if isinstance(q, unicode):
+                q = q.encode(charset)
+            self._query(q)
             self.nextset()
             
-        query = "CALL %s(%s)" % (procname,
-                                 ','.join(['@_%s_%d' % (procname, i)
-                                           for i in range(len(args))]))
-        if isinstance(query, unicode):
-            query = query.encode(charset)
-        self._query(query)
-        self._executed = query
-        if not self._defer_warnings:
-            self._warning_check()
+        q = "CALL %s(%s)" % (procname,
+                             ','.join(['@_%s_%d' % (procname, i)
+                                       for i in range(len(args))]))
+        if type(q) is UnicodeType:
+            q = q.encode(charset)
+        self._query(q)
+        self._executed = q
+        if not self._defer_warnings: self._warning_check()
         return args
     
-    def _do_query(self, query):
-        """Low-levey query wrapper. Overridden by MixIns."""
-        connection = self._get_db()
-        self._executed = query
-        connection.query(query)
+    def _do_query(self, q):
+        db = self._get_db()
+        self._last_executed = q
+        db.query(q)
         self._do_get_result()
         return self.rowcount
 
+    def _query(self, q): return self._do_query(q)
+    
     def _fetch_row(self, size=1):
-        """Low-level fetch_row wrapper."""
         if not self._result:
             return ()
         return self._result.fetch_row(size, self._fetch_type)
@@ -311,18 +292,33 @@ class Cursor(object):
     def __iter__(self):
         return iter(self.fetchone, None)
 
-    def _get_result(self):
-        """Low-level; uses mysql_store_result()"""
-        return self._get_db().store_result()
+    Warning = Warning
+    Error = Error
+    InterfaceError = InterfaceError
+    DatabaseError = DatabaseError
+    DataError = DataError
+    OperationalError = OperationalError
+    IntegrityError = IntegrityError
+    InternalError = InternalError
+    ProgrammingError = ProgrammingError
+    NotSupportedError = NotSupportedError
+   
 
-    def _query(self, query):
-        """Low-level; executes query, gets result, and returns rowcount."""
-        rowcount = self._do_query(query)
+class CursorStoreResultMixIn(object):
+
+    """This is a MixIn class which causes the entire result set to be
+    stored on the client side, i.e. it uses mysql_store_result(). If the
+    result set can be very large, consider adding a LIMIT clause to your
+    query, or using CursorUseResultMixIn instead."""
+
+    def _get_result(self): return self._get_db().store_result()
+
+    def _query(self, q):
+        rowcount = self._do_query(q)
         self._post_get_result()
         return rowcount
 
     def _post_get_result(self):
-        """Low-level"""
         self._rows = self._fetch_row(0)
         self._result = None
 
@@ -330,10 +326,9 @@ class Cursor(object):
         """Fetches a single row from the cursor. None indicates that
         no more rows are available."""
         self._check_executed()
-        if self.rownumber >= len(self._rows):
-            return None
+        if self.rownumber >= len(self._rows): return None
         result = self._rows[self.rownumber]
-        self.rownumber += 1
+        self.rownumber = self.rownumber+1
         return result
 
     def fetchmany(self, size=None):
@@ -364,19 +359,146 @@ class Cursor(object):
         value states an absolute target position."""
         self._check_executed()
         if mode == 'relative':
-            row = self.rownumber + value
+            r = self.rownumber + value
         elif mode == 'absolute':
-            row = value
+            r = value
         else:
-            self.errorhandler(self, self.ProgrammingError,
+            self.errorhandler(self, ProgrammingError,
                               "unknown scroll mode %s" % `mode`)
-        if row < 0 or row >= len(self._rows):
+        if r < 0 or r >= len(self._rows):
             self.errorhandler(self, IndexError, "out of range")
-        self.rownumber = row
+        self.rownumber = r
 
     def __iter__(self):
         self._check_executed()
         result = self.rownumber and self._rows[self.rownumber:] or self._rows
         return iter(result)
     
+
+class CursorUseResultMixIn(object):
+
+    """This is a MixIn class which causes the result set to be stored
+    in the server and sent row-by-row to client side, i.e. it uses
+    mysql_use_result(). You MUST retrieve the entire result set and
+    close() the cursor before additional queries can be peformed on
+    the connection."""
+
+    _defer_warnings = True
+    
+    def _get_result(self): return self._get_db().use_result()
+
+    def fetchone(self):
+        """Fetches a single row from the cursor."""
+        self._check_executed()
+        r = self._fetch_row(1)
+        if not r:
+            self._warning_check()
+            return None
+        self.rownumber = self.rownumber + 1
+        return r[0]
+             
+    def fetchmany(self, size=None):
+        """Fetch up to size rows from the cursor. Result set may be smaller
+        than size. If size is not defined, cursor.arraysize is used."""
+        self._check_executed()
+        r = self._fetch_row(size or self.arraysize)
+        self.rownumber = self.rownumber + len(r)
+        if not r:
+            self._warning_check()
+        return r
+         
+    def fetchall(self):
+        """Fetchs all available rows from the cursor."""
+        self._check_executed()
+        r = self._fetch_row(0)
+        self.rownumber = self.rownumber + len(r)
+        self._warning_check()
+        return r
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        row = self.fetchone()
+        if row is None:
+            raise StopIteration
+        return row
+    
+
+class CursorTupleRowsMixIn(object):
+
+    """This is a MixIn class that causes all rows to be returned as tuples,
+    which is the standard form required by DB API."""
+
     _fetch_type = 0
+
+
+class CursorDictRowsMixIn(object):
+
+    """This is a MixIn class that causes all rows to be returned as
+    dictionaries. This is a non-standard feature."""
+
+    _fetch_type = 1
+
+    def fetchoneDict(self):
+        """Fetch a single row as a dictionary. Deprecated:
+        Use fetchone() instead. Will be removed in 1.3."""
+        from warnings import warn
+        warn("fetchoneDict() is non-standard and will be removed in 1.3",
+             DeprecationWarning, 2)
+        return self.fetchone()
+
+    def fetchmanyDict(self, size=None):
+        """Fetch several rows as a list of dictionaries. Deprecated:
+        Use fetchmany() instead. Will be removed in 1.3."""
+        from warnings import warn
+        warn("fetchmanyDict() is non-standard and will be removed in 1.3",
+             DeprecationWarning, 2)
+        return self.fetchmany(size)
+
+    def fetchallDict(self):
+        """Fetch all available rows as a list of dictionaries. Deprecated:
+        Use fetchall() instead. Will be removed in 1.3."""
+        from warnings import warn
+        warn("fetchallDict() is non-standard and will be removed in 1.3",
+             DeprecationWarning, 2)
+        return self.fetchall()
+
+
+class CursorOldDictRowsMixIn(CursorDictRowsMixIn):
+
+    """This is a MixIn class that returns rows as dictionaries with
+    the same key convention as the old Mysqldb (MySQLmodule). Don't
+    use this."""
+
+    _fetch_type = 2
+
+
+class Cursor(CursorStoreResultMixIn, CursorTupleRowsMixIn,
+             BaseCursor):
+
+    """This is the standard Cursor class that returns rows as tuples
+    and stores the result set in the client."""
+
+
+class DictCursor(CursorStoreResultMixIn, CursorDictRowsMixIn,
+                 BaseCursor):
+
+     """This is a Cursor class that returns rows as dictionaries and
+    stores the result set in the client."""
+   
+
+class SSCursor(CursorUseResultMixIn, CursorTupleRowsMixIn,
+               BaseCursor):
+
+    """This is a Cursor class that returns rows as tuples and stores
+    the result set in the server."""
+
+
+class SSDictCursor(CursorUseResultMixIn, CursorDictRowsMixIn,
+                   BaseCursor):
+
+    """This is a Cursor class that returns rows as dictionaries and
+    stores the result set in the server."""
+
+
